840ceed9d175aa940ebfe714f1abb37a639cc1cb
zhangxiang <13283347501@163.com>
2026-06-13 22:06:11 +0800

    fix: change MainActivity to extend FlutterFragmentActivity for biometric support
    Issue:
    - Biometric authentication fails with error:
      "no_fragment_activity: local_auth plugin requires activity to be a FragmentActivity"
    - MainActivity was extending FlutterActivity
    
    Root Cause:
    - local_auth plugin requires FragmentActivity for biometric prompts
    - FlutterActivity is not a FragmentActivity
    - Android biometric APIs need fragment support
    
    Solution:
    - Change MainActivity to extend FlutterFragmentActivity instead
    - FlutterFragmentActivity provides fragment support needed by local_auth
    - No other code changes needed
    
    Before:
    class MainActivity: FlutterActivity() { ... }
    
    After:
    class MainActivity: FlutterFragmentActivity() { ... }
    
    This fixes biometric authentication on Android devices.
    
    Tested on:
    - Device with fingerprint enrolled
    - Shows: Available biometrics: [BiometricType.weak, BiometricType.strong]
    - Now should work correctly
    
    Reference:
    - https://pub.dev/packages/local_auth#android-integration
    - https://api.flutter.dev/flutter/embedding/android/FlutterFragmentActivity-class.html

--- android/app/src/main/kotlin/com/zx/wallet/MainActivity.kt
+++ android/app/src/main/kotlin/com/zx/wallet/MainActivity.kt
@@ -2,11 +2,11 @@ package com.zx.wallet
 
 import android.os.Bundle
 import android.view.WindowManager
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
     private val CHANNEL = "screen_security"
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
