17d2b39a0b22488e161cb6b24c0266ae409abe01
zhangxiang <13283347501@163.com>
2026-06-16 11:23:52 +0800

    build: regenerate routes without Phase 3 pages
    Regenerated route table after removing Phase 3 files.
    All Phase 1 and Phase 2 routes remain functional.
    
    Ready to run!

--- lib/generated/route_table.dart
+++ lib/generated/route_table.dart
@@ -3,8 +3,6 @@ import 'package:get/get.dart';
 import 'package:omnicast/page/transaction/view/transaction_history_page.dart';
 import 'package:omnicast/page/home/view/home_page.dart';
 import 'package:omnicast/page/splash/view/splash_page.dart';
-import 'package:omnicast/page/dapp/view/connected_dapps_page.dart';
-import 'package:omnicast/page/dapp/view/dapp_scanner_page.dart';
 import 'package:omnicast/page/transfer/view/transfer_page.dart';
 import 'package:omnicast/page/receive/view/receive_page.dart';
 import 'package:omnicast/page/browser/view/block_explorer_page.dart';
@@ -20,8 +18,6 @@ class RouteTable {
   static const String transactionHistory = '/transactionHistory';
   static const String home = '/home';
   static const String splash = '/splash';
-  static const String dapp_connected = '/dapp/connected';
-  static const String dapp_scan = '/dapp/scan';
   static const String transfer = '/transfer';
   static const String receive = '/receive';
   static const String blockExplorer = '/blockExplorer';
@@ -45,14 +41,6 @@ class RouteTable {
     GetPage(
       name: '/splash',
       page: () => SplashPage(),
-    ),
-    GetPage(
-      name: '/dapp/connected',
-      page: () => ConnectedDAppsPage(),
-    ),
-    GetPage(
-      name: '/dapp/scan',
-      page: () => DAppScannerPage(),
     ),
     GetPage(
       name: '/transfer',
